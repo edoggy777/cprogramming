20236cd94c17c87126ceda852f7174e78c18eb60
edoggy777 <kirtzevan@gmail.com>
2025-08-19 05:05:29 -0700

    More little programs

---
+++ 7. Arrays/arraybasic.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main () {
+   int a[10];
+   int b[10];
+   int i;
+
+   for (i = 0; i < 10; i++) {
+      scanf("%d", &a[i]);
+   }
+
+   for (i = 0; i < 10; i++) {
+      printf("%d\n", a[i]);
+   }
+
+   for (i = 0; i < 10; i++) {
+      b[i] = a[i];
+      printf("%d%d\n", b[i], a[i]);
+   } 
+}   
