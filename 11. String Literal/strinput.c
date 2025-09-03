adcd5b3d7543876644f72f97a2ea93a375db35ff
edoggy777 <kirtzevan@gmail.com>
2025-09-03 05:19:16 -0700

    More little programs

---
+++ 11. String Literal/strinput.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+   int LEN;
+   char words[LEN+1];
+
+   printf("Enter something: \n");
+   scanf("%s", words);
+   printf("%s\n", words);
+
+   return 0;
+}
---
+++ 11. String Literal/strlit.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+   char str[]= "The fun never stops";
+
+   printf("%s\n", str);
+
+   return 0;
+}
